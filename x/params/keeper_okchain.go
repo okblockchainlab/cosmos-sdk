a1e112bf36777ee72d0687488db11f7a82984a70
zhongqiuwood <zhongqiuwood@gmail.com>
2019-06-04 14:05:38 +0800

    move new methods into files with okchain suffix

---
+++ x/params/keeper_okchain.go
@@ -0,0 +1,22 @@
+package params
+
+func (k *Keeper) RegisterParamSet(paramSpace string, ps ...ParamSet) *Keeper {
+	for _, ps := range ps {
+		if ps != nil {
+			// if _, ok := paramSets[ps.GetParamSpace()]; ok {
+			// 	panic(fmt.Sprintf("<%s> already registered ", ps.GetParamSpace()))
+			// }
+			k.paramSets[paramSpace] = ps
+		}
+	}
+	return k
+}
+
+// Get existing substore from keeper
+func (k Keeper) GetParamSet(paramSpace string) (ParamSet, bool) {
+	paramSet, ok := k.paramSets[paramSpace]
+	if !ok {
+		return nil, false
+	}
+	return paramSet, ok
+}
---
+++ x/staking/types/params_okchain.go
@@ -0,0 +1,10 @@
+package types
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+
+func (p *Params) ValidateKV(key string, value string) (interface{}, sdk.Error) {
+	return nil, nil
+}
